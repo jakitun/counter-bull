82593e0be141b4cfc014f1ad5304602569a27d76
jakitun <jackytanable@gmail.com>
2026-04-18 23:58:17 +0700

    2.0

--- sw.js
+++ sw.js
@@ -1,18 +1,18 @@
-const CACHE_NAME = "oche-tracker-v1";
+const CACHE_NAME = "oche-v2";
 const urlsToCache = [
   "/",
   "/index.html",
   "/manifest.json"
 ];
 
-self.addEventListener("install", event => {
-  event.waitUntil(
+self.addEventListener("install", e => {
+  e.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
   );
 });
